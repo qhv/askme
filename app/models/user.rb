bcc88fdf93a26a3ccee1909800cd2490272468c6
Alex Dobrovolski <triiq@mail.ru>
2020-12-18 19:02:34 +0300

    add password feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,34 @@
-require 'openss'
+require 'openssl'
 
 class User < ApplicationRecord
+  ITERATIONS = 20_000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
   has_many :questions
 
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
+
+  attr_accessor :password
+
+  #old style
+  validates_presence_of :password, on: :create
+  validates_confirmation_of :password
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    return unless self.password.present?
+
+    self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+    self.password_hash = User.hash_to_string(
+      OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt,
+                                 ITERATIONS, DIGEST.length, DIGEST)
+    )
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
 end
